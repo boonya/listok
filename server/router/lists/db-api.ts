eb58f2999ef061f3cb13beb56a07725ed8268591
Serhii [boonya] Buinytskyi <779184+boonya@users.noreply.github.com>
2026-01-16 22:58:51 +0100

    Types refactored

--- server/router/lists/db-api.ts
+++ server/router/lists/db-api.ts
@@ -1,44 +1,44 @@
 import type {SupabaseClient} from '@/supabase-client';
 
-type Remove = {
+interface Remove {
   id: string;
   deleted_at: Date;
-}[];
-
-async function remove(supabase: SupabaseClient, input: Remove) {
-  // @ts-expect-error Just ignore it so far
-  await supabase.rpc('delete_outdated_lists', {items: input});
 }
 
-type Update = {
+async function remove(supabase: SupabaseClient, items: Remove[]) {
+  // @ts-expect-error Just ignore it so far
+  await supabase.rpc('delete_outdated_lists', {items});
+}
+
+interface Update {
   id: string;
   title: string;
   created_at: Date;
   updated_at: Date | null;
-}[];
+}
 
-async function update(supabase: SupabaseClient, input: Update) {
+async function update(supabase: SupabaseClient, input: Update[]) {
   const payload = input.map(({created_at, updated_at, ...rest}) => ({
     created_at: created_at.toISOString(),
     updated_at: updated_at?.toISOString() ?? null,
     ...rest,
   }));
-  await supabase.from('lists').upsert(payload).select('id');
+  await supabase.from('lists').upsert(payload);
 }
 
-type Create = {
+interface Create {
   title: string;
   created_at: Date;
   updated_at: Date | null;
-}[];
+}
 
-async function create(supabase: SupabaseClient, input: Create) {
+async function create(supabase: SupabaseClient, input: Create[]) {
   const payload = input.map(({created_at, updated_at, ...rest}) => ({
     created_at: created_at.toISOString(),
     updated_at: updated_at?.toISOString() ?? null,
     ...rest,
   }));
-  await supabase.from('lists').insert(payload).select('id');
+  await supabase.from('lists').insert(payload);
 }
 
 async function select(supabase: SupabaseClient) {
@@ -63,9 +63,9 @@ async function select(supabase: SupabaseClient) {
 
 export function getDbApi(supabase: SupabaseClient) {
   return {
-    remove: (input: Remove) => remove(supabase, input),
-    update: (input: Update) => update(supabase, input),
-    create: (input: Create) => create(supabase, input),
+    remove: (input: Remove[]) => remove(supabase, input),
+    update: (input: Update[]) => update(supabase, input),
+    create: (input: Create[]) => create(supabase, input),
     select: () => select(supabase),
   };
 }
