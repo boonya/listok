55165b25cf8d19a21856da369a8f969d07a6285a
Serhii [boonya] Buinytskyi <779184+boonya@users.noreply.github.com>
2026-01-18 16:07:31 +0100

    Client app env. vars and title

--- client/providers/theme/theme.tsx
+++ client/providers/theme/theme.tsx
@@ -1,14 +1,20 @@
 import {createTheme} from '@mui/material/styles';
 
+export const theme_color = '#6820dd';
+export const background_color = '#ffffff';
+
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#6820dd',
+      main: theme_color,
       contrastText: '#fff',
     },
     secondary: {
       main: '#ff7300',
       contrastText: '#fff',
+    },
+    background: {
+      default: background_color,
     },
   },
   // components: {
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,8 +5,12 @@ import {defineConfig, loadEnv} from 'vite';
 import {VitePWA} from 'vite-plugin-pwa';
 import tsConfigPaths from 'vite-tsconfig-paths';
 import z from 'zod';
+import {background_color, theme_color} from './providers/theme/theme';
 
 const EnvSchema = z.object({
+  APP_LANG: z.string().length(2),
+  NAME: z.string().min(3),
+  DESCRIPTION: z.string().min(10),
   REVISION: z.string().min(1),
   API_URL: z.url(),
   DEVSERVER_HOSTNAME: z.string().default('localhost'),
@@ -14,8 +18,16 @@ const EnvSchema = z.object({
 });
 
 export default defineConfig(({mode}) => {
-  const {REVISION, API_URL, DEVSERVER_PORT, DEVSERVER_HOSTNAME} =
-    EnvSchema.parse(loadEnv(mode, process.cwd(), ''));
+  const env = loadEnv(mode, process.cwd(), '');
+  const {
+    APP_LANG,
+    NAME,
+    DESCRIPTION,
+    REVISION,
+    API_URL,
+    DEVSERVER_PORT,
+    DEVSERVER_HOSTNAME,
+  } = EnvSchema.parse(env);
 
   return {
     define: {
@@ -41,6 +53,15 @@ export default defineConfig(({mode}) => {
       host: DEVSERVER_HOSTNAME,
     },
     plugins: [
+      {
+        name: 'transform-index.html',
+        transformIndexHtml(html) {
+          return html.replace(
+            /<title>(.*?)<\/title>/,
+            `<title>${NAME}</title>`,
+          );
+        },
+      },
       tsConfigPaths(),
       react(),
       VitePWA({
@@ -78,12 +99,12 @@ export default defineConfig(({mode}) => {
           ],
         },
         manifest: {
-          lang: 'uk',
-          name: 'Списки', // 👈 Use package.json, README.md or something like that
-          short_name: 'listock', // 👈 Use package.json, README.md or something like that
-          description: 'Менеджер списків', // 👈 Use package.json, README.md or something like that
-          theme_color: '#ffffff', // 👈 Use MUI theme defined values
-          background_color: '#ffffff', // 👈 Use MUI theme defined values
+          lang: APP_LANG,
+          name: NAME,
+          short_name: NAME,
+          description: DESCRIPTION,
+          theme_color,
+          background_color,
           display: 'standalone',
           icons: [
             {
